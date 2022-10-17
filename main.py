0f802b6ecbe9e9242abe2dc746eab23914a6fb25
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-10-17 02:03:26 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
-# ******************************
-# Make your Code
-# ******************************
+numbers = []
+for i in range(5):
+    numbers.append(int(input()))
+sum(numbers)
+avg = sum(numbers)/len(numbers)
+for i in range(5): 
+    if numbers[i] > avg:
+        print(numbers[i], end=' ')
